5df6feb527fa96bf2310a86c6441b62165f9c2db
Eneson <enersonsony7@gmail.com>
2023-02-04 12:19:30 -0300

    Update server.js
    teste

--- src/server.js
+++ src/server.js
@@ -8,13 +8,13 @@ const app = express()
 
 
 
-app.use(express.static('public')); 
-app.use(cors())
-app.use(express.json())
-app.get('/adote', (req, res) => res.json('teste'))
-// app.use(routes)
-// app.use(errors())
-// app.use('/uploads', express.static('uploads'));
+// app.use(express.static('public')); 
+// app.use(cors())
+// app.use(express.json())
+// app.get('/adote', (req, res) => res.json('teste'))
+app.use(routes)
+app.use(errors())
+app.use('/uploads', express.static('uploads'));
 
 
 app.listen(80)
