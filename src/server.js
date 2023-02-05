49fddcfb590f913e567168efcb4e0d0cf95e4b91
Eneson <enersonsony7@gmail.com>
2023-02-04 22:36:34 -0300

    update server.js express
    retirei o bug de request

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 // app.use(express.static('public')); 
 // app.use(cors())
-// app.use(express.json())
+app.use(express.json())
 // app.get('/adote', (req, res) => res.json('teste'))
 app.use(routes)
 app.use(errors())
