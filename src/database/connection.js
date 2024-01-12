730f3417caf85ec7f90db62e80b7ce219de90915
Eneson <enersonsony7@gmail.com>
2024-01-12 17:24:02 -0300

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,12 @@
-const knex = require('knex')
-const configuration = require('../../knexfile')
+const connection =  require('knex')({
+    client: 'mysql',
+    connection: {
+      host : 'host4069.hospedameusite.net',
+      port : 3306,
+      user : 'adotesto_01',
+      password : 'eE!20039807',
+      database : 'adotesto_01',
+    },
+})
 
-const config = process.env.NODE_ENV == 'test' ? configuration.test : configuration.development
-
-const connection = knex(config)
 module.exports = connection
