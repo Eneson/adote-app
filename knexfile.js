1542a3a868b0b21e298509054582bd0a1b4afd72
Eneson <enersonsony7@gmail.com>
2024-01-12 21:27:11 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,17 @@
 module.exports = {
     client: 'mysql',
     connection: {
-      host : 'host4069.hospedameusite.net',
-      port : 3306,
-      user : 'adotesto_01',
-      password : 'eE!20039807',      
-      database : 'adotesto_01',
-    },
-    pool: { min: 0, max: 7 }
+        host : 'r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        port : 3306,
+        user : 'kt300jk09xfh6hwb',
+        password : 'g2enom8u62x36sok',      
+        database : 'ul0soh4469aff5t0',
+      }
+    // connection: {
+    //   host : 'host4069.hospedameusite.net',
+    //   port : 3306,
+    //   user : 'adotesto_01',
+    //   password : 'eE!20039807',      
+    //   database : 'adotesto_01',
+    // }
 };
