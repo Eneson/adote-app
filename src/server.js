8a485dbe1bb44af9f28bb10329767cb0de20c34c
Eneson <enersonsony7@gmail.com>
2024-09-12 22:19:26 -0300

    Update server.js
    app.use(function (request, response, next) {
      response.header("Access-Control-Allow-Origin", "*");
      response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
      next();
    });

--- src/server.js
+++ src/server.js
@@ -10,6 +10,11 @@ const port = process.env.PORT || 3000;
 
 // app.use(express.static('public')); 
 // app.use(cors())
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json())
 app.use('/uploads', express.static('uploads'));
 // app.get('/adote', (req, res) => res.json('teste'))
