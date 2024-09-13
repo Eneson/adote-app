6dd7222ef430e101f07416fb512a2bacd6cf2f7d
Eneson <enersonsony7@gmail.com>
2024-09-13 16:20:18 -0300

    Access-Control-Allow-Headers

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(function (request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Methods", "*");
-  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  response.header("Access-Control-Allow-Headers", "*");
   next();
 });
 app.use(express.json())
