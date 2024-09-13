5bedfb0f90ce8414d224a2eb4badcf3b98c31be7
Eneson <enersonsony7@gmail.com>
2024-09-13 16:18:42 -0300

    Access-Control-Allow-Methods

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(function (request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", "*");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
